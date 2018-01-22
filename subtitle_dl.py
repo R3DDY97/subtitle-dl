6548f8a450fe41430ea84a1c35c70c744298cc35
R3DDY97 <venkyreddy097@gmail.com>
2018-01-22 15:09:39 +0530

    added banner , unzip srt

--- subtitle_dl.py
+++ subtitle_dl.py
@@ -12,30 +12,31 @@ from heading import banners
 
 # SEARCH = "the+blacklist"
 DOMAIN = "https://subscene.com/"
-BASE_URL = "https://subscene.com/subtitles/title"
-HEADERS = {"User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0"}
+URL = "https://subscene.com/subtitles/title"
+HDR = {"User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0"}
 
-
-# def search_term():
-#     term = input("\n\nEnter to search for subtitles (Eg:- game of thrones, Inception..) > ")
-#     query = "+".join(term.split())
-#     return {"q": query,}
-
-def search_results():
-    # params = search_term()
-    term = input("\n\nEnter to search for subtitles (Eg:- game of thrones, Inception..) > ")
+def search_srt():
+    term = input("\nEnter to search for subtitles (Eg:- game of thrones, Inception..) > ")
     query = "+".join(term.split())
-    response = requests.get(BASE_URL, params={"q": query,}, headers=HEADERS)
+    response = requests.get(URL, params={"q": query,}, headers=HDR)
     tree = html.fromstring(response.content)
+    results_found = tree.xpath(".//div[@class='search-result']/h2/text()")
+
+    if 'No results found' in results_found:
+        os.system("clear||cls")
+        banners()
+        print("No results found ..TRY using proper title")
+        search_srt()
+
     catg_items = tree.xpath(".//div/ul/li/div[@class='title']/a")
-    urls_cat = {urljoin(response.url, i.attrib["href"]):i.text for i in catg_items}
+    urls_cat = {urljoin(URL, i.attrib["href"]):i.text for i in catg_items}
     cat_urls = sorted([(value, key) for key, value in urls_cat.items()])
 
     while True:
         os.system("clear||cls")
         for num, item in enumerate(cat_urls, 1):
             print(f"\t[{num}]. {item[0]}")
-            sleep(0.5)
+            sleep(0.1)
         try:
             choosed = int(input("\nEnter NUMBER > "))
             if choosed <= len(cat_urls):
@@ -43,45 +44,42 @@ def search_results():
         except ValueError:
             pass
     print(f"\nYou have choosen {cat_urls[choosed-1][0]}")
-    return cat_urls[choosed-1][1]
+    subtitles_list(cat_urls[choosed-1][1])
 
 
-def subtitles_list():
-    url = search_results()
-    response = requests.get(url, headers=HEADERS)
-    # tree = html.fromstring(response.content)
-    # elements = tree.xpath(".//tbody/tr/td[@class='a1']")
+def subtitles_list(url):
+    response = requests.get(url, headers=HDR)
     elements = (html.fromstring(response.content)).xpath(".//tbody/tr/td[@class='a1']")
-    srt_info = [[urljoin(response.url, i.cssselect("a")[0].attrib["href"]),
+    srt_info = [[urljoin(url, i.cssselect("a")[0].attrib["href"]),
                  i.cssselect("span")[0].text.strip(),
-                 i.cssselect("span")[1].text.strip()] for i in elements]
+                 i.cssselect("span")[1].text.strip()] for i in elements] # ([url,lang, srt])
     lang_set = set([i.cssselect("span")[0].text.strip() for i in elements])
-    # srt_lang = [i.cssselect("span")[0].text.strip() for i in elements]
-    # srt_name = [i.cssselect("span")[1].text.strip() for i in elements]
-    # srt_ulrs = [urljoin(response.url, i.cssselect("a")[0].attrib["href"]) for i in elements]
-    # srt_dict = {url:[lang, name] for lang, name, url in zip(srt_lang, srt_name, srt_ulrs)}
-    srt_dict = {i[0]:i[1:] for i in srt_info}
-    srt_data = [[key, value] for key, value in srt_dict.items()]
+    # srt_dict = {i[0]:i[1:] for i in srt_info} # {url:[lang, srt]} to remove
+    # srt_data = [[key, value] for key, value in srt_dict.items()]
     lang_dict = collections.defaultdict()
 
     for lang in lang_set:
-        lang_dict[lang] = {j[0]:j[1][1] for j in srt_data if j[1][0] == lang}
+        lang_dict[lang] = {i[2]:i[0] for i in srt_info if i[1] == lang} #({srt_name:url})
 
-    #  Using ENGLISH Language now -
+    # for lang in lang_set:
+    #     lang_dict[lang] = {j[0]:j[1][1] for j in srt_data if j[1][0] == lang}
+
+    #  Using ENGLISH Language bydefault now -
     try:
-        eng_dict = lang_dict["English"]
+        eng_dict = lang_dict["English"]   #({srt_name:url})
         # srt_set = set(eng_dict.keys())
-        # srt_list = [(name,url)]
+        # srt_list = [[(name,url) for name, url in eng_dict[key]] for key in srt_set]
 
     except KeyError:
         os.system("clear||cls")
         print("\n English subtitles are not available for this....\n")
-        search_results()
+        search_srt()
 
-    eng_srt = sorted([(key, value) for key, value in eng_dict.items()])[::-1]
-
+    eng_srt = sorted([(url, srt_name) for srt_name, url in eng_dict.items()])[::-1]
     while True:
         os.system("clear||cls")
+        print(f"\n{len(eng_srt)} subtitles were found:\n")
+        sleep(1)
         for num, srt in enumerate(eng_srt, 1):
             print(f"\t[{num}]. {srt[1]}")
             sleep(0.1)
@@ -93,7 +91,7 @@ def subtitles_list():
             pass
     os.system("clear||cls")
     print("\n\n\tYou have choosen {}".format(eng_srt[chosen-1][1]))
-    return eng_srt[chosen-1][0]
+    download_srt(eng_srt[chosen-1][0])
 
 
     # # language selection
@@ -115,31 +113,29 @@ def subtitles_list():
 
 
 
-def main():
-    os.system("clear||cls")
-    banners()
-    srt_url = subtitles_list()
+def download_srt(srt_url):
     tree = html.fromstring(requests.get(srt_url).text)
     rel_dlink = tree.get_element_by_id("downloadButton").get("href")
     dlink = urljoin(DOMAIN, rel_dlink)
 
     # Download srt
-
-    filename = "{}.zip".format(srt_url.split("/")[-1])
+    cur_dir = os.path.abspath(os.getcwd())  # todo - download location
+    filename = "{0}/{1}.zip".format(cur_dir, srt_url.split("/")[-1])
     content = requests.get(dlink).content
     with open(filename, "wb") as srt:
         srt.write(content)
-        # print("\n\tDownload completed..!!") # to-d0  unzip it and ask for download location
     if zipfile.is_zipfile(filename):
         with zipfile.ZipFile(filename, 'r') as zip_ref:
             zip_ref.extractall()
-    print("\n\tDownload completed..!!") # to-d0  unzip it and ask for download location
-
-
-
+    os.remove(filename)
+    print(f"\n\tDownload completed..!! and \n\n\tsrt file is in:{cur_dir}")
+    input("\n\n\n\tPress any key to continue......")
 
 if __name__ == '__main__':
     try:
-        main()
+        while True:
+            os.system("clear||cls")
+            banners()
+            search_srt()
     except KeyboardInterrupt:
         print("\n\n\t Thank you....\n\n")
